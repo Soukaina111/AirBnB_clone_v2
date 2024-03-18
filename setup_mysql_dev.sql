db2b3acecead73a992ccdb94a4a0e9d547371bf6
Soukaina111 <basmahajar@gmail.com>
2024-03-18 11:13:59 -0700

    TASK3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,5 +7,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- This line used to grant all privileges on the hbnb_dev_db to database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
---This one is to  grant SELECT privilege on performance_schema to db
+-- This one is to  grant SELECT privilege on performance_schema to db
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
