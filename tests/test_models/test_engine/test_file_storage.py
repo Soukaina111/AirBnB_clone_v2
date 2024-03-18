a5e4726ed0d28dc534f0eab024dfd106a3d48fb4
Soukaina111 <basmahajar@gmail.com>
2024-03-18 14:37:02 -0700

    TESTS

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -107,3 +107,25 @@ class test_fileStorage(unittest.TestCase):
         from models.engine.file_storage import FileStorage
         print(type(storage))
         self.assertEqual(type(storage), FileStorage)
+
+    def test_create_with_params(self):
+        """Test creating objects with parameters"""
+        self.console.onecmd("create State name='California'")
+        self.assertEqual(len(storage.all()), 1)
+        self.assertIsInstance(list(storage.all().values())[0], State)
+        self.assertEqual(list(storage.all().values())[0].name, 'California')
+
+        self.console.onecmd("create Place city_id='0001' user_id='0001'
+                            name='My_little_house' number_rooms=4
+                            number_bathrooms=2
+                            max_guest=10 price_by_night=300")
+        self.assertEqual(len(storage.all()), 2)
+        self.assertIsInstance(list(storage.all().values())[1], Place)
+        place = list(storage.all().values())[1]
+        self.assertEqual(place.city_id, '0001')
+        self.assertEqual(place.user_id, '0001')
+        self.assertEqual(place.name, 'My little house')
+        self.assertEqual(place.number_rooms, 4)
+        self.assertEqual(place.number_bathrooms, 2)
+        self.assertEqual(place.max_guest, 10)
+        self.assertEqual(place.price_by_night, 300)
