3b114c54fadedaabfa08033fafc2cc44c39de2f0
Soukaina111 <basmahajar@gmail.com>
2024-03-19 09:05:58 -0700

    USERS

--- models/user.py
+++ models/user.py
@@ -5,16 +5,11 @@ from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """ This class defines a user by various attributes"""
-
-    """email = ''
-    password = ''
-    first_name = ''
-    last_name = ''"""
-
-
-__tablename__ = 'users'
-email = Column(String(128), nullable=False)
-password = Column(String(128), nullable=False)
-first_name = Column(String(128))
-last_name = Column(String(128))
+    """ This class defines a user
+    by various attributes
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
