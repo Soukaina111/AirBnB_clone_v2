b84bee114e86858c18fb9ea05ba46a96fa7de525
SaidLamghari <said.lmg@gmail.com>
2024-03-19 03:26:47 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -15,14 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage."""
-        obje = FileStorage.__objects
-        if cls is None:
-            return obje
-        target_cln = cls.__name__
-        fobj = {k: val for k, val in obje.items() if target_cln in k}
-        return fobj
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -57,22 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """Delete obj from __objects if it exists."""
-        if obj is None:
-            return
-
-        objects = FileStorage.__objects
-        to_delete = None
-
-        for k, val in objects.items():
-            if val == obj:
-                to_delete = key
-                break
-
-        if to_delete is not None:
-            del objects[to_delete]
