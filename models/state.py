ff142e4232f6bdf7846d6ad621ba5c4b2e30d78b
Soukaina111 <basmahajar@gmail.com>
2024-03-20 14:39:53 -0700

    FBSSS

--- models/state.py
+++ models/state.py
@@ -13,16 +13,22 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        @property
-        def cities(self):
-            '''
-            Returns a list of City instances with
-            state_id = State.id.
-            '''
 
-            c_dict = models.storage.all(City)
-            return [city for city in c_dict.values()
-                    if city.state_id == self.id]
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new State instance """
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            self.cities = relationship('City', cascade='all, delete', backref='state')
+        else:
+            @property
+            def cities(self):
+                '''
+                Returns a list of City instances with
+                state_id = State.id.
+                '''
+
+                c_dict = models.storage.all(City)
+                return [city for city in c_dict.values()
+                        if city.state_id == self.id]
+
