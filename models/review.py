4cc61d1f3b80aeb6c6303d591d3bdd087b296baa
Soukaina111 <basmahajar@gmail.com>
2024-03-20 16:54:49 -0700

    FBSSS

--- models/review.py
+++ models/review.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-<<<<<<< HEAD
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-
-class Review(BaseModel, Base):
-    """ Review classto store review information"""
-
-    __tablename__ = 'reviews'
-
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
-
-=======
 from models.base_model import BaseModel
 
 
@@ -22,4 +8,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
->>>>>>> 1de38e50eb65c77bcb0c043241c9e69e1dbef266
