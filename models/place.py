277fe1309a4600287544a4f53104544a8b3a8177
Soukaina111 <basmahajar@gmail.com>
2024-03-20 16:47:49 -0700

    FBSSS

--- models/place.py
+++ models/place.py
@@ -3,20 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
-class Place(BaseModel, Base):
-    """ A place to stay 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []"""
 
+class Place(BaseModel, Base):
+    """ A place to stay """
+    
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
