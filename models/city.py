9980e98a31248f719749cb900b91187df740eea5
Soukaina111 <basmahajar@gmail.com>
2024-03-20 15:07:58 -0700

    FBSSS

--- models/city.py
+++ models/city.py
@@ -11,3 +11,9 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new City instance """
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
+        self.state_id = kwargs.get('state_id', '')
