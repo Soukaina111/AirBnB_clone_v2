3c1bfe64d0c37cbfa1999563046d19abb0a03506
Soukaina111 <basmahajar@gmail.com>
2024-03-20 15:20:44 -0700

    FBSSS

--- models/user.py
+++ models/user.py
@@ -15,3 +15,11 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new User instance """
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', '')
+        self.password = kwargs.get('password', '')
+        self.first_name = kwargs.get('first_name', '')
+        self.last_name = kwargs.get('last_name', '')
