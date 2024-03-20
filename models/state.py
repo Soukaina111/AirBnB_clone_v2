90fbc3843e3a000378b1538ff111496a68998ab8
Soukaina111 <basmahajar@gmail.com>
2024-03-20 00:33:16 -0700

    FS vs DBS2

--- models/state.py
+++ models/state.py
@@ -12,8 +12,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship('City', cascade='all, delete', backref='state')
     else:
         @property
