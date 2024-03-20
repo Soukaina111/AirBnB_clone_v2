1847da432f03e8175fc0b3897cfcbb286bd39777
SaidLamghari <said.lmg@gmail.com>
2024-03-20 10:35:52 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-import os
 import models
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-
     name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        @property
-        def cities(self):
-            '''
-            Returns a list of City instances with
-            state_id = the current State.id.
-            '''
-
-            c_dict = models.storage.all(City)
-            return [city for city in c_dict.values()
-                    if city.state_id == self.id]
+    @property
+    def cities(self):
+        InVr = models.storage.all()
+        Mylst = []
+        rslt = []
+        for clek in InVr:
+            cty = clek.replace('.', ' ')
+            cty = shlex.split(cty)
+            if cty[0] == 'City':
+                Mylst.append(InVr[clek])
+        idx = 0
+        while idx < len(Mylst):
+            eleitm = Mylst[idx]
+            if eleitm.state_id == self.id:
+                rslt.append(eleitm)
+            idx += 1
+        return rslt
