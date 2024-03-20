b254d5c9a8d461c92fcac0a09d93460cb0816dc0
Soukaina111 <basmahajar@gmail.com>
2024-03-20 16:40:30 -0700

    FBSSS

--- models/review.py
+++ models/review.py
@@ -2,14 +2,9 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models.users import User
-from models.places import Place
 
 class Review(BaseModel, Base):
-    """ Review classto store review information 
-    place_id = ""
-    user_id = ""
-    text = """
+    """ Review classto store review information"""
 
     __tablename__ = 'reviews'
 
