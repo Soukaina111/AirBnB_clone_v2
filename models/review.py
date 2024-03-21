0baf7feb8ec5cc76753391c35fc0d5fa5b70dcb7
SaidLamghari <said.lmg@gmail.com>
2024-03-20 17:56:35 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from models.base_model import BaseModel, Base
 from sqlalchemy import Integer, ForeignKey
 from sqlalchemy import Column, String
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    
+ 
     __tablename__ = 'reviews'
 
     text = Column("text", String(1024), nullable=False)
