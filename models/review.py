c220f5487d70711cd3ef450c277dfa7ab957f632
SaidLamghari <said.lmg@gmail.com>
2024-03-20 17:57:59 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -10,6 +10,7 @@ class Review(BaseModel):
     __tablename__ = 'reviews'
 
     text = Column("text", String(1024), nullable=False)
+    
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
