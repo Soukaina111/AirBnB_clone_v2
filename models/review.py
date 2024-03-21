3dbdb6e2411623994835c2e380637abebbde7ec6
SaidLamghari <said.lmg@gmail.com>
2024-03-20 18:17:44 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
 
 class Review(BaseModel):
     """ Review classto store review information """
