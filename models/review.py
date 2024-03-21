443bcdf8bb6a236d27ce41eb4d585d38f87ee995
SaidLamghari <said.lmg@gmail.com>
2024-03-20 18:01:37 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -7,6 +7,5 @@ from sqlalchemy import Column, String
 class Review(BaseModel):
     """ Review classto store review information """
  
-    __tablename__ = 'reviews'
 
     text = Column("text", String(1024), nullable=False)
