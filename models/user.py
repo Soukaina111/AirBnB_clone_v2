ffbdfc145d9339cf39a2bcb636dbc3d2024ad3f2
SaidLamghari <said.lmg@gmail.com>
2024-03-20 17:40:01 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.review import Review
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 import os
