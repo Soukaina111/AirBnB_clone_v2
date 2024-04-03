4a9c7992ed28ef47ee0ba95afbfb6dd71cd0a9ac
Soukaina111 <basmahajar@gmail.com>
2024-04-03 15:15:06 -0700

    v2

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# This script prepares the web servers for deployement
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html><body><h1>Hello, Souka</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+echo "server {
+    listen 80;
+    server_name easypath.tech;
+
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        try_files \$uri \$uri/ =404;
+    }
+}" | sudo tee /etc/nginx/sites-available/hbnb_static > /dev/null
+
+sudo ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+
+echo " Nginx is configured to serve content from /data/web_static/current/ at https://easypath.tech/hbnb_static"
+
