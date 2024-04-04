3c6a37ecb4d61a4dda63565238c55da64fa9d5d5
Soukaina111 <basmahajar@gmail.com>
2024-04-04 14:27:38 -0700

    NEEW

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,78 @@
 #!/usr/bin/python3
-# This script uses  fabfile to distribute an archive to a web server.
+""" This script answers this: Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers
+using the function do_deploy
+"""
+from fabric.api import *
+from fabric.operations import put
+from datetime import datetime
 import os
-from fabric.api import env, put, run
 
 env.hosts = ["52.87.254.240", "54.237.1.11"]
 
 
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+def do_pack():
+    """ This function packages all thefile coming from web-static to tgz
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name_offi = file.split(".")[0]
+    n = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    outc = local('tar -cvf versions/web_static_{}.tgz web_static'
+                   .format(n))
+    if outc.failed:
+        return None
+    else:
+        return outc
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+
+def do_deploy(archive_path):
+    """ This function deploys a backup file web_01 and web_02"""
+
+    if not os.path.isfile(archive_path):
+        print('archive file does not exist...')
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name_offi)).failed is True:
+    try:
+        backup = archive_path.split('/')[1]
+        no_archive_ext = backup.split('.')[0]
+    except Exception:
+        print('failed to get archive name from split...')
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name_offi)).failed is True:
+    file2 = put(archive_path, '/tmp/')
+    if file2.failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name_offi)).failed is True:
+    nex = run('mkdir -p /data/web_static/releases/{}/'.format(no_archive_ext))
+    if nex.failed:
+        print('failed to create archive directory for relase...')
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    nex = run('tar -C /data/web_static/releases/{} -xzf /tmp/{}'.format(
+               no_archive_ext, archive))
+    if nex.failed:
+        print('failed to untar archive...')
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name_offi,
-               name_offi)).failed is True:
+    nex = run('rm /tmp/{}'.format(archive))
+    if nex.failed:
+        print('failed to remove archive...')
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name_offi)).failed is True:
+    nex = run('mv /data/web_static/releases/{}/web_static/* \
+               /data/web_static/releases/{}'
+              .format(no_archive_ext, no_archive_ext))
+    if nex.failed:
+        print('failed to move extraction to proper directory...')
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+   nex = run('rm -rf /data/web_static/releases/{}/web_static'
+              .format(no_archive_ext))
+    if nex.failed:
+        print('failed to remove first copy of extraction after move...')
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name_offi)).failed is True:
+    nex = run('rm -rf /data/web_static/current')
+    if nex.failed:
+        print('failed to clean up old release...')
         return False
+    nex = run('ln -sfn /data/web_static/releases/{} /data/web_static/current'
+              .format(no_archive_ext))
+    if nex.failed:
+        print('failed to create link to new release...')
+        return False
+
+    print('\n Congrats! A new Version is now available!\n')
+
     return True
