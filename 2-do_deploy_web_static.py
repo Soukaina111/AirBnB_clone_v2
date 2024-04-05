2310682cacd8cab251c0f53e7de9483de8626a20
Soukaina111 <basmahajar@gmail.com>
2024-04-04 22:58:21 -0700

    DEPLOY

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
 # This script uses  fabfile to distribute an archive to a web server.
-import os
-from fabric.api import env, put, run
+
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+import os.path
 
 env.hosts = ["52.87.254.240", "54.237.1.11"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
+    '''distributes an archive to your web servers
+    using the function do_deploy'''
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
-    name_offi = file.split(".")[0]
+    call = file.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(name_offi)).failed is True:
+            format(call)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name_offi)).failed is True:
+            format(call)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name_offi)).failed is True:
+            format(file, call)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name_offi,
-               name_offi)).failed is True:
+           "/data/web_static/releases/{}/".format(call, call)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name_offi)).failed is True:
+           format(call)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name_offi)).failed is True:
+           format(call)).failed is True:
         return False
     return True
