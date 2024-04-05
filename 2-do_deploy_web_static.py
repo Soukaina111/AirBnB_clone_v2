329bf497778c51125cc552624b6cdea9746ea6f3
Soukaina111 <basmahajar@gmail.com>
2024-04-05 14:56:05 -0700

    H

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python3
-# This script uses  fabfile to distribute an archive to a web server.
-
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+# Fabfile to distribute an archive to a web server.
 import os.path
+from fabric.api import env, put, run
 
 env.hosts = ["52.87.254.240", "54.237.1.11"]
 
 
 def do_deploy(archive_path):
-    '''distributes an archive to your web servers
-    using the function do_deploy'''
-    if os.path.isfile(archive_path) is False:
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isfile(archive_path):
         return False
     file = archive_path.split("/")[-1]
-    call = file.split(".")[0]
+    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-            format(call)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-            format(call)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}".format(name)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file, call)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file, name)).failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(call, call)).failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(call)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(call)).failed is True:
+    if run("ln -s /data/web_static/releases/{} /data/web_static/current".format(name)).failed:
         return False
     return True
+
