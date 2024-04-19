963316b7d351d6a254d75c9e9a2e373476d26684
Soukaina111 <basmahajar@gmail.com>
2024-04-19 08:18:27 -0700

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/",strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ Hello HBNB """
     return "Hello HBNB!"
