ecf2f2d8643c2c28c601fda21e1362d79e5b60f5
Soukaina111 <basmahajar@gmail.com>
2024-04-19 08:29:10 -0700

    fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,20 +1,23 @@
-
 #!/usr/bin/python3
 """ Let's get started with Flask"""
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ hello HBNB"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hello_world2():
     """ hello 2"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
@@ -22,12 +25,14 @@ def c(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def c2(text):
     """ Text2 with spaces"""
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
