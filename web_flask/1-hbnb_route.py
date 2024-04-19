7613d9feb3d80430d0eded0a78f7e643fa8d696f
Soukaina111 <basmahajar@gmail.com>
2024-04-19 08:20:02 -0700

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,17 +3,21 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ hello HBNB"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hello_world2():
     """ hello 2"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
