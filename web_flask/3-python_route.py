7b83f1792940523e4365cf4ec1c2490ab31d9e36
Soukaina111 <basmahajar@gmail.com>
2024-04-19 08:26:40 -0700

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,23 +3,28 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ hello HBNB"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hello_world2():
     """ hello 2"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ Text with spaces"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
