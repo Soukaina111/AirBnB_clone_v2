37170c179d3b4c8965054c7ab92b4fc70537bb47
Soukaina111 <basmahajar@gmail.com>
2024-04-19 08:21:54 -0700

    fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,23 +3,28 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ hello HBNB"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hello_world2():
     """ hello 2"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ Text with spaces"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
